202c5cafff78d71a6e986d78143b33495f716363
Anton Suprunchuk <antouhou@gmail.com>
2020-12-23 18:20:23 +0300

    fix: chainlock hash endianness (#208)
    BREAKING CHANGE: ChainLock#hash is now big-endian

--- test/chainlock/chainlock.js
+++ test/chainlock/chainlock.js
@@ -50,7 +50,7 @@ describe('ChainLock', function () {
       signature: '0a43f1c3e5b3e8dbd670bca8d437dc25572f72d8e1e9be673e9ebbb606570307c3e5f5d073f7beb209dd7e0b8f96c751060ab3a7fb69a71d5ccab697b8cfa5a91038a6fecf76b7a827d75d17f01496302942aa5e2c7f4a48246efc8d3941bf6c'
     };
     buf2 = Buffer.from(str2, 'hex');
-    expectedHash2 = "3764ada6c32f09bb4f02295415b230657720f8be17d6fe046f0f8bf3db72b8e0";
+    expectedHash2 = "e0b872dbf38b0f6f04fed617bef820776530b2155429024fbb092fc3a6ad6437";
     expectedRequestId2 = "5d92e094e2aa582b76e8bf519f42c5e8fc141bbe548e9660726f744adad03966";
 
     // DashSync test vectors : https://github.com/dashevo/dashsync-iOS/blob/master/Example/Tests/DSChainLockTests.m
